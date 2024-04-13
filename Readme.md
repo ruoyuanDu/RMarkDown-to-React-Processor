bfaaf7839aa1e11b9ebe56e0ec3423aea0fc3697
phoebe_doo <57498371+ruoyuanDu@users.noreply.github.com>
2024-04-13 13:04:40 -0400

    add mkdn_reactProcessor_RVisualization.py

--- Readme.md
+++ Readme.md
@@ -6,6 +6,8 @@
 3. In terminal run: python3 mkdn_reactProcessor_RGallery.py --input_folder_path './output'
 or
     In terminal run: python3 mkdn_reactProcessor_dplyr.py --input_folder_path './output'
+or 
+    In terminal run: python3 mkdn_reactProcessor_RVisualization.py --input_folder_path './output'
     this will take files from the output folder and create new react.js files in the outputReact folder.  
     To process a single file in the input folder, use reactProcessor.py input_fiel <file_name(no path needed as long as it's in the output folder)>. The file should be in the output folder too.
 
--- data.js
+++ data.js
@@ -1,92 +1,42 @@
 import React from 'react';
-import Ggplot2HeatmapVaccineForPolio from '../RgalleryPages/contents/ggplot2-heatmap-vaccine-for-polio_output_react'
-import Ggplot2ArrowsFacetWomenParliament from '../RgalleryPages/contents/ggplot2-arrows-facet-women-parliament_output_react'
-import Ggplot2MapAirline from '../RgalleryPages/contents/ggplot2-map-airline_output_react'
-import Ggplot2ArtsyRArtworks from '../RgalleryPages/contents/ggplot2-aRtsy-R-artworks_output_react'
-import Ggplot2BarsReorderedCars from '../RgalleryPages/contents/ggplot2-bars-reordered-cars_output_react'
-import Ggplot22dHistogramStormActivitiesUs from '../RgalleryPages/contents/ggplot2-2D-histogram-storm-activities-US_output_react'
-import Ggplot2BarEmojiHappyMoney from '../RgalleryPages/contents/ggplot2-bar-Emoji-happy-money_output_react'
-import Ggplot2WafflePlotWealthDistribution from '../RgalleryPages/contents/ggplot2-waffle-plot-wealth-distribution_output_react'
-import Ggplot2LinePlotGapminder from '../RgalleryPages/contents/ggplot2-line-plot-gapminder_output_react'
-import Ggplot2DonutGdp from '../RgalleryPages/contents/ggplot2-donut-GDP_output_react'
-import Ggplot2LollipopDonutHorsepower from '../RgalleryPages/contents/ggplot2-lollipop-donut-horsepower_output_react'
-import Ggplot2ScatterplotUrbanization from '../RgalleryPages/contents/ggplot2-scatterplot-urbanization_output_react'
-import Ggplot2HeatmapVolcanoTopography from '../RgalleryPages/contents/ggplot2-heatmap-volcano-topography_output_react'
-import Ggplot2MapFijiEarthquakes from '../RgalleryPages/contents/ggplot2-map-fiji-earthquakes_output_react'
-import Ggplot2GeofacetUsStates from '../RgalleryPages/contents/ggplot2-geofacet-US-states_output_react'
-import Ggplot2BarMortality from '../RgalleryPages/contents/ggplot2-bar-mortality_output_react'
-import Ggplot2ScatterplotTechniques from '../RgalleryPages/contents/ggplot2-scatterplot-techniques_output_react'
-import Ggplot2LinePlotSocialMobility from '../RgalleryPages/contents/ggplot2-line-plot-social-mobility_output_react'
-import Ggplot2LineSummaryStatisticsMilk from '../RgalleryPages/contents/ggplot2-line-summary-statistics-milk_output_react'
-import Ggplot2BarPolarCoordHorsepower from '../RgalleryPages/contents/ggplot2-bar-polar-coord-horsepower_output_react'
-import Ggplot2BarCo2AirTravel from '../RgalleryPages/contents/ggplot2-bar-CO2-air-travel_output_react'
-import Ggplot22dHistogramHappy from '../RgalleryPages/contents/ggplot2-2D-histogram-happy_output_react'
-import Ggplot2PopulationPyramid from '../RgalleryPages/contents/ggplot2-population-pyramid_output_react'
-import Ggplot2DrawWithMathFunctions from '../RgalleryPages/contents/ggplot2-draw-with-math-functions_output_react'
-import Ggplot2RadarWine from '../RgalleryPages/contents/ggplot2-radar-wine_output_react'
-import Ggplot2BarsMidwestCounty from '../RgalleryPages/contents/ggplot2-bars-midwest-county_output_react'
-import Ggplot2LinePlotCigarettes from '../RgalleryPages/contents/ggplot2-line-plot-cigarettes_output_react'
-import Ggplot2DumbellPlotVotesTrumpClinton from '../RgalleryPages/contents/ggplot2-dumbell-plot-votes-Trump-Clinton_output_react'
-import Ggplot2LinePlotLifeExpectancy from '../RgalleryPages/contents/ggplot2-line-plot-life-expectancy_output_react'
-import Ggplot2HeatmapSunspotActivities from '../RgalleryPages/contents/ggplot2-heatmap-sunspot-activities_output_react'
-import Ggplot2ScatterplotDiamonds from '../RgalleryPages/contents/ggplot2-scatterplot-diamonds_output_react'
-import Ggplot2HeatmapAfricanPopulation from '../RgalleryPages/contents/ggplot2-heatmap-African-population_output_react'
-import Ggplot2WorldMapCitiesPopulations from '../RgalleryPages/contents/ggplot2-world-map-cities-populations_output_react'
-import Ggplot2SegmentUsPresidentTerms from '../RgalleryPages/contents/ggplot2-segment-US-president-terms_output_react'
-import Ggplot2StreamBlockbuster from '../RgalleryPages/contents/ggplot2-stream-blockbuster_output_react'
-import Ggplot2AreaPlotMigration from '../RgalleryPages/contents/ggplot2-area-plot-migration_output_react'
-import Ggplot2DonutPlotAdmission from '../RgalleryPages/contents/ggplot2-donut-plot-admission_output_react'
-import Ggplot2DensityPlotGgridgeSunspot from '../RgalleryPages/contents/ggplot2-density-plot-ggridge-sunspot_output_react'
-import Ggplot2MapAirlineAnimation from '../RgalleryPages/contents/ggplot2-map-airline-animation_output_react'
-import Ggplot2HeatmapVaccineForDiseases from '../RgalleryPages/contents/ggplot2-heatmap-vaccine-for-8-diseases_output_react'
-import Ggplot2BarSleep from '../RgalleryPages/contents/ggplot2-bar-sleep_output_react'
-import Ggplot2PenguinScatterplotEllipseGgextra from '../RgalleryPages/contents/ggplot2-penguin-scatterplot-ellipse-ggExtra_output_react'
-import Ggplot2WafflePlotChernoffFacesWealth from '../RgalleryPages/contents/ggplot2-Waffle-plot-Chernoff-faces-wealth_output_react'
-import Ggplot2MosaicHealthWealth from '../RgalleryPages/contents/ggplot2-mosaic-health-wealth_output_react'
-import Ggplot2PopulationPyramidAnimation from '../RgalleryPages/contents/ggplot2-population-pyramid-animation_output_react'
-const data=[{'component': '<Ggplot2HeatmapVaccineForPolio />', 'path': 'ggplot2-heatmap-vaccine-for-polio', 'title': 'ggplot2 heatmap vaccine for polio'},
-{'component': '<Ggplot2ArrowsFacetWomenParliament />', 'path': 'ggplot2-arrows-facet-women-parliament', 'title': 'ggplot2 arrows facet women parliament'},
-{'component': '<Ggplot2MapAirline />', 'path': 'ggplot2-map-airline', 'title': 'ggplot2 map airline'},
-{'component': '<Ggplot2ArtsyRArtworks />', 'path': 'ggplot2-aRtsy-R-artworks', 'title': 'ggplot2 aRtsy R artworks'},
-{'component': '<Ggplot2BarsReorderedCars />', 'path': 'ggplot2-bars-reordered-cars', 'title': 'ggplot2 bars reordered cars'},
-{'component': '<Ggplot22dHistogramStormActivitiesUs />', 'path': 'ggplot2-2D-histogram-storm-activities-US', 'title': 'ggplot2 2D histogram storm activities US'},
-{'component': '<Ggplot2BarEmojiHappyMoney />', 'path': 'ggplot2-bar-Emoji-happy-money', 'title': 'ggplot2 bar Emoji happy money'},
-{'component': '<Ggplot2WafflePlotWealthDistribution />', 'path': 'ggplot2-waffle-plot-wealth-distribution', 'title': 'ggplot2 waffle plot wealth distribution'},
-{'component': '<Ggplot2LinePlotGapminder />', 'path': 'ggplot2-line-plot-gapminder', 'title': 'ggplot2 line plot gapminder'},
-{'component': '<Ggplot2DonutGdp />', 'path': 'ggplot2-donut-GDP', 'title': 'ggplot2 donut GDP'},
-{'component': '<Ggplot2LollipopDonutHorsepower />', 'path': 'ggplot2-lollipop-donut-horsepower', 'title': 'ggplot2 lollipop donut horsepower'},
-{'component': '<Ggplot2ScatterplotUrbanization />', 'path': 'ggplot2-scatterplot-urbanization', 'title': 'ggplot2 scatterplot urbanization'},
-{'component': '<Ggplot2HeatmapVolcanoTopography />', 'path': 'ggplot2-heatmap-volcano-topography', 'title': 'ggplot2 heatmap volcano topography'},
-{'component': '<Ggplot2MapFijiEarthquakes />', 'path': 'ggplot2-map-fiji-earthquakes', 'title': 'ggplot2 map fiji earthquakes'},
-{'component': '<Ggplot2GeofacetUsStates />', 'path': 'ggplot2-geofacet-US-states', 'title': 'ggplot2 geofacet US states'},
-{'component': '<Ggplot2BarMortality />', 'path': 'ggplot2-bar-mortality', 'title': 'ggplot2 bar mortality'},
-{'component': '<Ggplot2ScatterplotTechniques />', 'path': 'ggplot2-scatterplot-techniques', 'title': 'ggplot2 scatterplot techniques'},
-{'component': '<Ggplot2LinePlotSocialMobility />', 'path': 'ggplot2-line-plot-social-mobility', 'title': 'ggplot2 line plot social mobility'},
-{'component': '<Ggplot2LineSummaryStatisticsMilk />', 'path': 'ggplot2-line-summary-statistics-milk', 'title': 'ggplot2 line summary statistics milk'},
-{'component': '<Ggplot2BarPolarCoordHorsepower />', 'path': 'ggplot2-bar-polar-coord-horsepower', 'title': 'ggplot2 bar polar coord horsepower'},
-{'component': '<Ggplot2BarCo2AirTravel />', 'path': 'ggplot2-bar-CO2-air-travel', 'title': 'ggplot2 bar CO2 air travel'},
-{'component': '<Ggplot22dHistogramHappy />', 'path': 'ggplot2-2D-histogram-happy', 'title': 'ggplot2 2D histogram happy'},
-{'component': '<Ggplot2PopulationPyramid />', 'path': 'ggplot2-population-pyramid', 'title': 'ggplot2 population pyramid'},
-{'component': '<Ggplot2DrawWithMathFunctions />', 'path': 'ggplot2-draw-with-math-functions', 'title': 'ggplot2 draw with math functions'},
-{'component': '<Ggplot2RadarWine />', 'path': 'ggplot2-radar-wine', 'title': 'ggplot2 radar wine'},
-{'component': '<Ggplot2BarsMidwestCounty />', 'path': 'ggplot2-bars-midwest-county', 'title': 'ggplot2 bars midwest county'},
-{'component': '<Ggplot2LinePlotCigarettes />', 'path': 'ggplot2-line-plot-cigarettes', 'title': 'ggplot2 line plot cigarettes'},
-{'component': '<Ggplot2DumbellPlotVotesTrumpClinton />', 'path': 'ggplot2-dumbell-plot-votes-Trump-Clinton', 'title': 'ggplot2 dumbell plot votes Trump Clinton'},
-{'component': '<Ggplot2LinePlotLifeExpectancy />', 'path': 'ggplot2-line-plot-life-expectancy', 'title': 'ggplot2 line plot life expectancy'},
-{'component': '<Ggplot2HeatmapSunspotActivities />', 'path': 'ggplot2-heatmap-sunspot-activities', 'title': 'ggplot2 heatmap sunspot activities'},
-{'component': '<Ggplot2ScatterplotDiamonds />', 'path': 'ggplot2-scatterplot-diamonds', 'title': 'ggplot2 scatterplot diamonds'},
-{'component': '<Ggplot2HeatmapAfricanPopulation />', 'path': 'ggplot2-heatmap-African-population', 'title': 'ggplot2 heatmap African population'},
-{'component': '<Ggplot2WorldMapCitiesPopulations />', 'path': 'ggplot2-world-map-cities-populations', 'title': 'ggplot2 world map cities populations'},
-{'component': '<Ggplot2SegmentUsPresidentTerms />', 'path': 'ggplot2-segment-US-president-terms', 'title': 'ggplot2 segment US president terms'},
-{'component': '<Ggplot2StreamBlockbuster />', 'path': 'ggplot2-stream-blockbuster', 'title': 'ggplot2 stream blockbuster'},
-{'component': '<Ggplot2AreaPlotMigration />', 'path': 'ggplot2-area-plot-migration', 'title': 'ggplot2 area plot migration'},
-{'component': '<Ggplot2DonutPlotAdmission />', 'path': 'ggplot2-donut-plot-admission', 'title': 'ggplot2 donut plot admission'},
-{'component': '<Ggplot2DensityPlotGgridgeSunspot />', 'path': 'ggplot2-density-plot-ggridge-sunspot', 'title': 'ggplot2 density plot ggridge sunspot'},
-{'component': '<Ggplot2MapAirlineAnimation />', 'path': 'ggplot2-map-airline-animation', 'title': 'ggplot2 map airline animation'},
-{'component': '<Ggplot2HeatmapVaccineForDiseases />', 'path': 'ggplot2-heatmap-vaccine-for-8-diseases', 'title': 'ggplot2 heatmap vaccine for 8 diseases'},
-{'component': '<Ggplot2BarSleep />', 'path': 'ggplot2-bar-sleep', 'title': 'ggplot2 bar sleep'},
-{'component': '<Ggplot2PenguinScatterplotEllipseGgextra />', 'path': 'ggplot2-penguin-scatterplot-ellipse-ggExtra', 'title': 'ggplot2 penguin scatterplot ellipse ggExtra'},
-{'component': '<Ggplot2WafflePlotChernoffFacesWealth />', 'path': 'ggplot2-Waffle-plot-Chernoff-faces-wealth', 'title': 'ggplot2 Waffle plot Chernoff faces wealth'},
-{'component': '<Ggplot2MosaicHealthWealth />', 'path': 'ggplot2-mosaic-health-wealth', 'title': 'ggplot2 mosaic health wealth'},
-{'component': '<Ggplot2PopulationPyramidAnimation />', 'path': 'ggplot2-population-pyramid-animation', 'title': 'ggplot2 population pyramid animation'},
+import Ggplot2WrapLongTexts from '../RVisualization/contents/3-ggplot2-wrap-long-texts_output_react'
+import Ggplot2ReorderViolinPlots from '../RVisualization/contents/5-ggplot2-reorder-violin-plots_output_react'
+import Ggplot2ReorderStackingOfBars from '../RVisualization/contents/7-ggplot2-reorder-stacking-of-bars_output_react'
+import Ggplot2TitleLegendRemoval from '../RVisualization/contents/13-2-ggplot2-title-legend-removal_output_react'
+import Ggplot2RenameTitleLegend from '../RVisualization/contents/13-ggplot2-rename-title-legend_output_react'
+import Ggplot2ReorderLegendKeys from '../RVisualization/contents/8-ggplot2-reorder-legend-keys_output_react'
+import Ggplot2OverlapErrorbar from '../RVisualization/contents/3-ggplot2-overlap-errorbar_output_react'
+import Ggplot2RelocateLegendPosition from '../RVisualization/contents/10-ggplot2-relocate-legend-position_output_react'
+import Ggplot2ChangeLegendRowNumber from '../RVisualization/contents/11-ggplot2-change-legend-row-number_output_react'
+import Ggplot2CreatePoints from '../RVisualization/contents/18-ggplot2-create-points_output_react'
+import Ggplot2ReorderLegendColorbar from '../RVisualization/contents/9-ggplot2-reorder-legend-colorBar_output_react'
+import Ggplot2ReorderMultipleLegends from '../RVisualization/contents/12-ggplot2-reorder-multiple-legends_output_react'
+import Ggplot2ColorBrewerPalette from '../RVisualization/contents/15-ggplot2-color-brewer-palette_output_react'
+import Ggplot2ReorderBars from '../RVisualization/contents/4-ggplot2-reorder-bars_output_react'
+import Ggplot2ColorViridisPalette from '../RVisualization/contents/16-ggplot2-color-viridis-palette_output_react'
+import Ggplot2OvercrowdedDataPoints from '../RVisualization/contents/1-ggplot2-overcrowded-data-points_output_react'
+import Ggplot2ReorderFacetedPanels from '../RVisualization/contents/6-ggplot2-reorder-faceted-panels_output_react'
+import Ggplot2OvercrowdedTextLabels from '../RVisualization/contents/2-ggplot2-overcrowded-text-labels_output_react'
+import Ggplot2LegendRemove from '../RVisualization/contents/14-ggplot2-legend-remove_output_react'
+import Ggplot2PolarCoordSummary from '../RVisualization/contents/17-ggplot2-polar-coord-summary_output_react'
+const data=[{'component': '<Ggplot2WrapLongTexts />', 'path': '3-ggplot2-wrap-long-texts', 'title': '3 ggplot2 wrap long texts'},
+{'component': '<Ggplot2ReorderViolinPlots />', 'path': '5-ggplot2-reorder-violin-plots', 'title': '5 ggplot2 reorder violin plots'},
+{'component': '<Ggplot2ReorderStackingOfBars />', 'path': '7-ggplot2-reorder-stacking-of-bars', 'title': '7 ggplot2 reorder stacking of bars'},
+{'component': '<Ggplot2TitleLegendRemoval />', 'path': '13-2-ggplot2-title-legend-removal', 'title': '13 2 ggplot2 title legend removal'},
+{'component': '<Ggplot2RenameTitleLegend />', 'path': '13-ggplot2-rename-title-legend', 'title': '13 ggplot2 rename title legend'},
+{'component': '<Ggplot2ReorderLegendKeys />', 'path': '8-ggplot2-reorder-legend-keys', 'title': '8 ggplot2 reorder legend keys'},
+{'component': '<Ggplot2OverlapErrorbar />', 'path': '3-ggplot2-overlap-errorbar', 'title': '3 ggplot2 overlap errorbar'},
+{'component': '<Ggplot2RelocateLegendPosition />', 'path': '10-ggplot2-relocate-legend-position', 'title': '10 ggplot2 relocate legend position'},
+{'component': '<Ggplot2ChangeLegendRowNumber />', 'path': '11-ggplot2-change-legend-row-number', 'title': '11 ggplot2 change legend row number'},
+{'component': '<Ggplot2CreatePoints />', 'path': '18-ggplot2-create-points', 'title': '18 ggplot2 create points'},
+{'component': '<Ggplot2ReorderLegendColorbar />', 'path': '9-ggplot2-reorder-legend-colorBar', 'title': '9 ggplot2 reorder legend colorBar'},
+{'component': '<Ggplot2ReorderMultipleLegends />', 'path': '12-ggplot2-reorder-multiple-legends', 'title': '12 ggplot2 reorder multiple legends'},
+{'component': '<Ggplot2ColorBrewerPalette />', 'path': '15-ggplot2-color-brewer-palette', 'title': '15 ggplot2 color brewer palette'},
+{'component': '<Ggplot2ReorderBars />', 'path': '4-ggplot2-reorder-bars', 'title': '4 ggplot2 reorder bars'},
+{'component': '<Ggplot2ColorViridisPalette />', 'path': '16-ggplot2-color-viridis-palette', 'title': '16 ggplot2 color viridis palette'},
+{'component': '<Ggplot2OvercrowdedDataPoints />', 'path': '1-ggplot2-overcrowded-data-points', 'title': '1 ggplot2 overcrowded data points'},
+{'component': '<Ggplot2ReorderFacetedPanels />', 'path': '6-ggplot2-reorder-faceted-panels', 'title': '6 ggplot2 reorder faceted panels'},
+{'component': '<Ggplot2OvercrowdedTextLabels />', 'path': '2-ggplot2-overcrowded-text-labels', 'title': '2 ggplot2 overcrowded text labels'},
+{'component': '<Ggplot2LegendRemove />', 'path': '14-ggplot2-legend-remove', 'title': '14 ggplot2 legend remove'},
+{'component': '<Ggplot2PolarCoordSummary />', 'path': '17-ggplot2-polar-coord-summary', 'title': '17 ggplot2 polar coord summary'},
 ]
